81597e5a523fb6d384584aa266efaa5976e9e0f0
DarkCBZ <91396838+DarkCBZ@users.noreply.github.com>
2023-09-22 13:59:40 -0500

    pls work

--- index.js
+++ index.js
@@ -13,12 +13,18 @@ try {
 
     ctx.fillRect(0, 0, Math.ceil(canvas.width), Math.ceil(canvas.height));
 
-      ctx.lineWidth = 1;
+    ctx.lineWidth = 1;
     for (let i = 0; i < canvas.width; i += 30) {
-        ctx.beginPath();
-        ctx.moveTo(i, 0);
-        ctx.lineTo(i, canvas.height);
-        ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(i, 0);
+      ctx.lineTo(i, canvas.height);
+      ctx.stroke();
+    }
+    for (let i = 0; i < canvas.height; i += 30) {
+      ctx.beginPath();
+      ctx.moveTo(0, i);
+      ctx.lineTo(canvas.width, i);
+      ctx.stroke();
     }
   }
 

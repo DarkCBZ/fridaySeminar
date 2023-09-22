103b2ad86e963d508d15ba60aee73f86e241188f
DarkCBZ <91396838+DarkCBZ@users.noreply.github.com>
2023-09-22 10:21:35 -0500

    wh

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillRect(0, 0, 100, 100);
+ctx.fillRect(0, 0, document.width, document.height);
 
 alert(canvas.width, canvas.height);
 alert("test");

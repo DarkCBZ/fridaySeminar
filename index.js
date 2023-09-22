bfc7d7cd774cb4ba6ef5ded0daef7097c90c5375
DarkCBZ <91396838+DarkCBZ@users.noreply.github.com>
2023-09-22 16:09:35 -0500

    updates

--- index.js
+++ index.js
@@ -1,42 +1,71 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-function tick() {
-  try {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    //alert(canvas.width);
-    //alert(canvas.height);
+function render() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-    //Background Start
-    ctx.fillStyle = "#B22222";
+  ctx.fillStyle = "#B22222";
 
-    ctx.fillRect(0, 0, Math.ceil(canvas.width), Math.ceil(canvas.height));
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.lineWidth = 1;
-    for (let i = 0; i < canvas.width; i += 30) {
-      ctx.beginPath();
-      ctx.moveTo(i, 0);
-      ctx.lineTo(i, canvas.height);
-      ctx.stroke();
-    }
-    for (let i = 0; i < canvas.height; i += 30) {
-      ctx.beginPath();
-      ctx.moveTo(0, i);
-      ctx.lineTo(canvas.width, i);
-      ctx.stroke();
-    }
-  } catch (error) {
-    alert(error);
+  ctx.lineWidth = 1;
+  for (let i = 0; i < canvas.width; i += 30) {
+    ctx.beginPath();
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, canvas.height);
+    ctx.stroke();
   }
+  for (let i = 0; i < canvas.height; i += 30) {
+    ctx.beginPath();
+    ctx.moveTo(0, i);
+    ctx.lineTo(canvas.width, i);
+    ctx.stroke();
+  }
+
+  ctx.lineWidth = 1;
+  ctx.fillStyle = player.color;
+  ctx.fillRect(player.x, player.y, player.width, player.height);
+
+  //top
+  ctx.beginPath();
+  ctx.moveTo(player.x, player.y);
+  ctx.lineTo(player.x + player.width, player.y);
+  ctx.stroke();
+
+  //bottom
+  ctx.beginPath();
+  ctx.moveTo(player.x + player.width, player.y + player.height);
+  ctx.lineTo(player.x, player.y + player.height);
+  ctx.stroke();
+
+  //left
+  ctx.beginPath();
+  ctx.moveTo(player.x, player.y + player.height);
+  ctx.lineTo(player.x, player.y);
+  ctx.stroke();
+
+  //right
+  ctx.beginPath();
+  ctx.moveTo(player.x + player.width, player.y);
+  ctx.lineTo(player.x + player.width, player.y + player.height);
+  ctx.stroke();
 }
-//Background End
+let player = {
+  x: 20,
+  y: 20,
+  width: 50,
+  height: 50,
+  color: "#303030"
+}
+function tick() {
+
+  render();
+}
 
 window.onresize = () => {
-  //alert("Resized");
-  tick();
+  render();
 };
 window.onload = () => {
-  //alert("Loaded");
-  tick();
+  render();
 };
